ced1f59201f9b8a026fd518e4c80a1c247ea0305
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-03-23 12:08:41 +0100

    chore: type changes

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,6 @@
 export type Options = {
-  suffix: string;
-  assetsDir: string | undefined;
-  wrapper: string;
-  svgo: boolean;
+  suffix?: string;
+  assetsDir?: string | undefined;
+  wrapper?: string;
+  svgo?: boolean;
 };
